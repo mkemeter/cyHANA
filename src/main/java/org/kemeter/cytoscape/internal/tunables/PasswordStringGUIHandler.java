4bdcb37b352838d9afafcc383de61a6e6ba8d398
Mathias Kemeter <kemeter@gmail.com>
2021-09-23 13:09:06 +0200

    Add Cytoscape copyright

--- src/main/java/org/kemeter/cytoscape/internal/tunables/PasswordStringGUIHandler.java
+++ src/main/java/org/kemeter/cytoscape/internal/tunables/PasswordStringGUIHandler.java
@@ -16,6 +16,31 @@ import org.slf4j.LoggerFactory;
 
 import static org.kemeter.cytoscape.internal.tunables.GUIDefaults.*;
 
+
+/*
+ * 
+ * Cytoscape Work Swing Impl (work-swing-impl)
+ * 
+ *  
+ * Copyright (C) 2006 - 2021 The Cytoscape Consortium
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * 
+ */
+
+
 /**
  * Slightly modified version of
  * https://github.com/cytoscape/cytoscape-impl/blob/develop/work-swing-impl/impl/src/main/java/org/cytoscape/work/internal/tunables/StringHandler.java
