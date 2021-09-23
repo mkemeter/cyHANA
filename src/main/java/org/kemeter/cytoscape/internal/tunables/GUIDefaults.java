e82107082f833955136dff7e3d08dee296f1960e
Mathias Kemeter <kemeter@gmail.com>
2021-09-23 13:07:49 +0200

    Add Cytoscape Copyright

--- src/main/java/org/kemeter/cytoscape/internal/tunables/GUIDefaults.java
+++ src/main/java/org/kemeter/cytoscape/internal/tunables/GUIDefaults.java
@@ -8,6 +8,30 @@ import java.awt.*;
 import static javax.swing.GroupLayout.DEFAULT_SIZE;
 import static javax.swing.GroupLayout.PREFERRED_SIZE;
 
+/*
+ * 
+ * Cytoscape Work Swing Impl (work-swing-impl)
+ * 
+ *  
+ * Copyright (C) 2006 - 2021 The Cytoscape Consortium
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * 
+ */
+
+
 /**
  * Contents of this class are copied from
  * org.cytoscape.work.internal.tunables.utils.GUIDefaults
