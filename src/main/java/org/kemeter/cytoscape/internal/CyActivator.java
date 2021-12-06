5c74bcf79f1d03609efdaa293013a66de7965172
Mathias Kemeter <mathias.kemeter@sap.com>
2021-12-06 20:24:13 +0100

    Moving menu configuration to properties file

--- src/main/java/org/kemeter/cytoscape/internal/CyActivator.java
+++ src/main/java/org/kemeter/cytoscape/internal/CyActivator.java
@@ -12,16 +12,21 @@ import org.kemeter.cytoscape.internal.tasks.CyLoadTaskFactory;
 import org.kemeter.cytoscape.internal.tasks.CyRefreshTaskFactory;
 import org.kemeter.cytoscape.internal.tunables.PasswordString;
 import org.kemeter.cytoscape.internal.tunables.PasswordStringGUIHandler;
+import org.kemeter.cytoscape.internal.utils.IOUtils;
 import org.osgi.framework.BundleContext;
 import org.kemeter.cytoscape.internal.hdb.HanaConnectionManager;
 import org.kemeter.cytoscape.internal.tasks.CyConnectTaskFactory;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class CyActivator extends AbstractCyActivator {
 
-    public CyActivator(){
+    private final Properties menuConfiguration;
+
+    public CyActivator() throws IOException {
         super();
+        this.menuConfiguration = IOUtils.loadResourceProperties("MenuConfiguration.properties");
     }
 
     public void start(BundleContext bc) {
@@ -40,33 +45,33 @@ public class CyActivator extends AbstractCyActivator {
             // connect
             CyConnectTaskFactory connectFactory = new CyConnectTaskFactory(connectionManager);
             Properties connectProps = new Properties();
-            connectProps.setProperty(ServiceProperties.PREFERRED_MENU, "Apps.SAP HANA");
-            connectProps.setProperty(ServiceProperties.TITLE, "Connect to Database");
-            connectProps.setProperty(ServiceProperties.MENU_GRAVITY, "1.0");
+            connectProps.setProperty(ServiceProperties.PREFERRED_MENU, this.menuConfiguration.getProperty("CONNECT_PREFERRED_MENU"));
+            connectProps.setProperty(ServiceProperties.TITLE, this.menuConfiguration.getProperty("CONNECT_TITLE"));
+            connectProps.setProperty(ServiceProperties.MENU_GRAVITY, this.menuConfiguration.getProperty("CONNECT_MENU_GRAVITY"));
             registerService(bc, connectFactory, TaskFactory.class, connectProps);
 
             // create graph workspace from network
             CyCreateWorkspaceTaskFactory createFactory = new CyCreateWorkspaceTaskFactory(networkManager, connectionManager);
             Properties createProps = new Properties();
-            createProps.setProperty(ServiceProperties.PREFERRED_MENU, "Apps.SAP HANA");
-            createProps.setProperty(ServiceProperties.TITLE, "Create Graph Workspace from Current Network");
-            createProps.setProperty(ServiceProperties.MENU_GRAVITY, "2.0");
+            createProps.setProperty(ServiceProperties.PREFERRED_MENU, this.menuConfiguration.getProperty("CREATE_PREFERRED_MENU"));
+            createProps.setProperty(ServiceProperties.TITLE, this.menuConfiguration.getProperty("CREATE_TITLE"));
+            createProps.setProperty(ServiceProperties.MENU_GRAVITY, this.menuConfiguration.getProperty("CREATE_MENU_GRAVITY"));
             registerService(bc, createFactory, TaskFactory.class, createProps);
 
             // load graph workspace
             CyLoadTaskFactory loadFactory = new CyLoadTaskFactory(networkFactory, networkManager, connectionManager);
             Properties loadProps = new Properties();
-            loadProps.setProperty(ServiceProperties.PREFERRED_MENU, "Apps.SAP HANA");
-            loadProps.setProperty(ServiceProperties.TITLE, "Load Graph Workspace from Database");
-            loadProps.setProperty(ServiceProperties.MENU_GRAVITY, "3.0");
+            loadProps.setProperty(ServiceProperties.PREFERRED_MENU, this.menuConfiguration.getProperty("LOAD_PREFERRED_MENU"));
+            loadProps.setProperty(ServiceProperties.TITLE, this.menuConfiguration.getProperty("LOAD_TITLE"));
+            loadProps.setProperty(ServiceProperties.MENU_GRAVITY, this.menuConfiguration.getProperty("LOAD_MENU_GRAVITY"));
             registerService(bc, loadFactory, TaskFactory.class, loadProps);
 
             // refresh current network from SAP HANA
             CyRefreshTaskFactory refreshFactory = new CyRefreshTaskFactory(networkFactory, networkManager, connectionManager);
             Properties refreshProps = new Properties();
-            refreshProps.setProperty(ServiceProperties.PREFERRED_MENU, "Apps.SAP HANA");
-            refreshProps.setProperty(ServiceProperties.TITLE, "Refresh Current Network from Database");
-            refreshProps.setProperty(ServiceProperties.MENU_GRAVITY, "4.0");
+            refreshProps.setProperty(ServiceProperties.PREFERRED_MENU, this.menuConfiguration.getProperty("REFRESH_PREFERRED_MENU"));
+            refreshProps.setProperty(ServiceProperties.TITLE, this.menuConfiguration.getProperty("REFRESH_TITLE"));
+            refreshProps.setProperty(ServiceProperties.MENU_GRAVITY, this.menuConfiguration.getProperty("REFRESH_MENU_GRAVITY"));
             registerService(bc, refreshFactory, TaskFactory.class, refreshProps);
 
             // load result of openCypher query
