c832aab017d506bb0ed4f959b560619ddf3ddcde
Mathias Kemeter <mathias.kemeter@sap.com>
2022-02-04 16:08:02 +0100

    Replacing README with redirection to new repository

--- src/main/java/org/kemeter/cytoscape/internal/exceptions/HanaConnectionManagerException.java
+++
@@ -1,9 +0,0 @@
-package org.kemeter.cytoscape.internal.exceptions;
-
-
-
-public class HanaConnectionManagerException extends Exception{
-    public HanaConnectionManagerException(String message){
-        super(message);
-    }
-}
